83a682b67080bdd0568b4d20827361a601b356cf
Brainsink <brainsink@gmail.com>
2018-08-30 15:49:01 +0200

    Use Express to Create a Server - attempt2

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Index'
+    });
+});
+
 app.listen(PORT,() => {
     console.log(`PS Project Running on port ${PORT}!`);
 });
